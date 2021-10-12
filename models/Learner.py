cf1d54332e7d2751e148d6098a877886668b8e8f
kwekyichen <73012648+jefferykyc@users.noreply.github.com>
2021-10-12 20:22:00 +0800

    trainer and learner update..

--- models/Learner.py
+++ models/Learner.py
@@ -17,17 +17,36 @@ class Learner(Engineer):
     __tablename__ = 'learner'
 
     learner_id = db.Column(db.Integer, primary_key = True)
-    engineer_id = db.Column(db.Integer, nullable=False)
+    engineer_id = db.Column(db.Integer, db.ForeignKey(Engineer.engineer_id), nullable=False)
     
     def __init__(self, engineer_id = "", learner_id = ""):
         super().__init__(engineer_id)
         self.learner_id = learner_id
+
+    def json(self):
+        return{"engineer_id": self.engineer_id, "engineer_name": self.engineer_name, "learner_id": self.learner_id}
 
     def get_learner_id(self):
         return self.learner_id
 
     def set_learner_id(self, learner_id):
         self.learner_id = learner_id
+        
+@app.route("/all", methods=["GET"])
+def get_all():
+
+    learnerlist = Learner.query.all()
+    
+    if len(learnerlist):
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "learner": [learner.json() for learner in learnerlist]
+                }
+            }
+        )
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5005, debug=True)
