e549a278fff1c76dc22ca7ac3b35a286cf9a82c2
muhd-asnawi <muhdasnaaawi@gmail.com>
2021-10-14 18:34:45 +0800

    Unit testing file added
    - Unit testing works
    - .json() method tested for Course, Class, Lesson, Material classes
    - minor fixes for the json() method for Course and Class
    - minor fix of string values of material_id & uploader to int values in material.sql

--- database/material.sql
+++ database/material.sql
@@ -18,8 +18,8 @@ CREATE TABLE IF NOT EXISTS `material` (
 );
 
 INSERT INTO `material` (`material_id`, `material_name`, `format`, `material_type`, `uploader`, `course_name`, `class_id`, `lesson_id`) VALUES
-('1', 'Material 1', NULL, 'Quiz', '100001', 'Course 111', 1, 1),
-('2', 'Material 2', NULL, 'Learning Package', '100002', 'Course 111', 1, 1), 
-('3', 'Material 3', NULL, 'Quiz', '100002', 'Course 111', 1, 2),
-('4', 'Material 4', NULL, 'Quiz', '100001', 'Course 222', 1, 4),
-('5', 'Material 5', NULL, 'Learning Package', '100002', 'Course 222', 1, 4);
+(1, 'Material 1', NULL, 'Quiz', 100001, 'Course 111', 1, 1),
+(2, 'Material 2', NULL, 'Learning Package', 100002, 'Course 111', 1, 1), 
+(3, 'Material 3', NULL, 'Quiz', 100002, 'Course 111', 1, 2),
+(4, 'Material 4', NULL, 'Quiz', 100001, 'Course 222', 1, 4),
+(5, 'Material 5', NULL, 'Learning Package', 100002, 'Course 222', 1, 4);
