f6b37df341719489dfe4956137b25511615daa45
kwekyichen <73012648+jefferykyc@users.noreply.github.com>
2021-10-22 22:17:26 +0800

    jenkin 11 change according to error flagged flake8

--- models/administrator.py
+++ models/administrator.py
@@ -1,14 +1,14 @@
 # Administrator
-#jenkintest11
+# jenkintest11
 class Administrator:
     
-    def __init__(self, name = "", administrator_id = ""):
+    def __init__(self, name="", administrator_id=""):
         self.name = name
         self.administrator_id = administrator_id
 
     def get_name(self):
         return self.name
-    
+   
     def set_name(self, name):
         self.name = name
 
@@ -16,5 +16,4 @@ class Administrator:
         return self.administrator_id
 
     def set_administrator_id(self, administrator_id):
-        self.administrator_id = administrator_id
-        
+        self.administrator_id = administrator_id        
