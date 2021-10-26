e54a268ba4fbe4cca5611c3f0599af4a60560b67
kwekyichen <73012648+jefferykyc@users.noreply.github.com>
2021-10-26 16:41:34 +0800

    test jenkins 1

--- models/learner_class.py
+++ models/learner_class.py
@@ -220,7 +220,7 @@ def get_pending_count():
             }
         )
 
-# test jenkin part 10 :(
+# test jenkin part 1
 # update pending status
 @app.route("/pending/<int:learner_id>/<string:course_name>", methods=["PUT"])
 def update_pending(learner_id, course_name):
