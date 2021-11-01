5f8e4ece9674a4e9281d5dde3ea37b09db98982d
md-asnawi <90689216+md-asnawi@users.noreply.github.com>
2021-11-01 17:25:47 +0800

    testing jenkins 6

--- models/unit_tests.py
+++ models/unit_tests.py
@@ -2,9 +2,9 @@ import unittest
 import datetime
 
 from course import Course
-# from Class import Class
-# from lesson import Lesson
-# from material import Material
+from Class import Class
+from lesson import Lesson
+from material import Material
 # testing jenkin3
 # Test for class Course
 class TestCourse(unittest.TestCase):
@@ -26,6 +26,7 @@ class TestClass(unittest.TestCase):
         class1 = Class('Course 111', 1, 40, datetime.date(2021, 1, 1),
                         datetime.date(2021, 4, 1), datetime.time(8, 15),
                         datetime.time(11, 30), 'Deez')
+        
         self.assertEqual(class1.json(), {
                 'course_name': 'Course 111',
                 'class_id': 1,
@@ -41,31 +42,30 @@ class TestClass(unittest.TestCase):
 # Test for class Lesson
 class TestLesson(unittest.TestCase):
     def test_to_json(self):
-        lesson1 = Lesson('Course 111', 1, 1, True, 75)
+        lesson1 = Lesson('Course 111', 1, 1, "This is Course 111")
         self.assertEqual(lesson1.json(), {
                 'course_name': 'Course 111',
                 'class_id': 1,
                 'lesson_id': 1,
-                'ungraded_quiz': True,
-                'completion_status': 75
+                "description": "This is Course 111"
             }
         )
 
 # Test for class Material
-class TestMaterial(unittest.TestCase):
-    def test_to_json(self):
-        material1 = Material(1, 'Material 1', '', 'Quiz', 100001, 'Course 111', 1, 1)
-        self.assertEqual(material1.json(), {
-                'material_id': 1,
-                'material_name': 'Material 1',
-                'format': '',
-                'material_type': 'Quiz',
-                'uploader': 100001,
-                'course_name': 'Course 111',
-                'class_id': 1,
-                'lesson_id': 1
-            }
-        )
+# class TestMaterial(unittest.TestCase):
+#     def test_to_json(self):
+#         material1 = Material(1, 'Material 1', '', 'Quiz', 100001, 'Course 111', 1, 1)
+#         self.assertEqual(material1.json(), {
+#                 'material_id': 1,
+#                 'material_name': 'Material 1',
+#                 'format': '',
+#                 'material_type': 'Quiz',
+#                 'uploader': 100001,
+#                 'course_name': 'Course 111',
+#                 'class_id': 1,
+#                 'lesson_id': 1
+#             }
+#         )
 
 if __name__ == "__main__":
     unittest.main()
