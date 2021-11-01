f7ca1033f0c2b1f96a06b7cabc5964768b2beb71
kwekyichen <73012648+jefferykyc@users.noreply.github.com>
2021-11-01 17:04:05 +0800

    test jenkins 1

--- models/unit_tests.py
+++ models/unit_tests.py
@@ -5,7 +5,7 @@ from course import Course
 # from Class import Class
 # from lesson import Lesson
 # from material import Material
-
+# testing jenkin
 # Test for class Course
 class TestCourse(unittest.TestCase):
     def test_to_json(self):
