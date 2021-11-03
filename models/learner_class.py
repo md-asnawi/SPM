836c300c719d9b5702a038c4335dc14d55aab62c
md-asnawi <90689216+md-asnawi@users.noreply.github.com>
2021-11-03 18:24:04 +0800

    Withdrawal bugs
    1. Withdrawal message saved to db and update withdrawal status is done using the same endpoint
    2. Can only withdraw if they self-enrol, not preassigned

--- models/learner_class.py
+++ models/learner_class.py
@@ -144,16 +144,72 @@ def get_courses_by_learner_id(learner_id):
             }
         ), 404
 
+# get all non-preassigned courses learner enrolled in using learner id
+@app.route("/learner_class/withdrawal/<int:learner_id>", methods=["GET"])
+def get_non_preassigned_courses_by_learner_id(learner_id):
+
+    learner_class_list = Learner_Class(db.Model).query.filter_by(learner_id=learner_id, enrolment_status="Enrolled", preassigned=0, withdrawal=0).all()
+ 
+    if len(learner_class_list):
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "learner_class": [learner_class.json() for learner_class in learner_class_list]
+                }
+            }
+        )
+
+    return jsonify(
+            {
+                "message": "Learner courses not found."
+            }
+        ), 404
+
 # learner_class/update_withdrawal/
 # update withdrawal status
+# only can withdraw if course is not preassigned by HR
+# actually this one not used, if refactoring delete this
 @app.route("/learner_class/update_withdrawal/<int:learner_id>/<string:course_name>", methods=["PUT"])
-def update_withdrawal(learner_id, course_name):
+def update_withdrawal_status(learner_id, course_name):
 
-    learner_class = Learner_Class(db.Model).query.filter_by(learner_id=learner_id, course_name=course_name).first()
+    learner_class = Learner_Class(db.Model).query.filter_by(learner_id=learner_id, course_name=course_name, preassigned = 0).first()
     
     if learner_class:
         # data = request.get_json()
         # if data['withdrawal']:
+        learner_class.withdrawal = True
+        db.session.commit()
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "learner_class": learner_class.json()
+                }
+            }
+        )
+
+    return jsonify(
+            {
+                "code": 404,
+                "data": {
+                    "learner_id": learner_id,
+                    "course_name": course_name
+                },
+                "message": "Learner or Class not found"
+            }
+        ), 404
+
+
+@app.route("/learner_class/withdrawal_message/<int:learner_id>/<string:course_name>", methods=["PUT"])
+def update_withdrawal_message(learner_id, course_name):
+
+    learner_class = Learner_Class(db.Model).query.filter_by(learner_id=learner_id, course_name=course_name, preassigned = 0).first()
+    
+    if learner_class:
+        data = request.get_json()
+        # if data['withdrawal']:
+        learner_class.withdrawal_message = data['withdrawal_message']
         learner_class.withdrawal = True
         db.session.commit()
         return jsonify(
