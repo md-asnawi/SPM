65c7cdca85125b7f14c6e29840518e5ea93a88e6
kwekyichen <73012648+jefferykyc@users.noreply.github.com>
2021-11-04 19:01:14 +0800

    edited the course name

--- database/learner_lesson.sql
+++ database/learner_lesson.sql
@@ -16,6 +16,6 @@ CREATE TABLE IF NOT EXISTS `learner_lesson` (
 );
 
 INSERT INTO `learner_lesson` (`course_name`, `class_id`, `learner_id`, `lesson_id`, `is_completed`) VALUES
-('Course 111', 1, 999, 1, TRUE),
-('Course 111', 1, 999, 2, FALSE),
-('Course 111', 1, 999, 3, FALSE);
+('Ink Course', 1, 999, 1, TRUE),
+('Ink Course', 1, 999, 2, FALSE),
+('Ink Course', 1, 999, 3, FALSE);
